9d1de29081e80022aa1335359b0e3abe1761cfee
Isaias Neres Pinto <isaias.neres@yahoo.com>
2019-03-20 23:50:56 -0300

    Testando a primeira classe de dominio - Categorias

---
+++ src/main/java/com/isaias/cursomc/domain/Categoria.java
@@ -0,0 +1,74 @@
+package com.isaias.cursomc.domain;
+
+import java.io.Serializable;
+
+public class Categoria implements Serializable{
+	/* Interface Serializable: para que os objetos da
+	 * Classe sejam convertidos em uma sequencia
+	 * de bytes: para que os objetos sejam gravados em arquivo,
+	 * trafegados em rede
+	 */
+	
+	private static final long serialVersionUID = 1L;
+		
+	private Integer id;
+	private String nome;
+	
+	//posso instaciar a classe sem precisar inserir dados nos campos
+	public Categoria() {
+	}
+
+	public Categoria(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+	//hashCode e equal (usa-se para comprar objetos pelo conteudo)
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+
+}
