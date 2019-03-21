eeeae9d88b3ad46fd3dd6d0891e2ea0d9a3cd3e8
Isaias Neres Pinto <isaias.neres@yahoo.com>
2019-03-21 01:50:00 -0300

    Banco de dados H2 e da criação da entity

--- src/main/java/com/isaias/cursomc/domain/Categoria.java
+++ src/main/java/com/isaias/cursomc/domain/Categoria.java
@@ -2,6 +2,12 @@ package com.isaias.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable{
 	/* Interface Serializable: para que os objetos da
 	 * Classe sejam convertidos em uma sequencia
@@ -10,7 +16,9 @@ public class Categoria implements Serializable{
 	 */
 	
 	private static final long serialVersionUID = 1L;
-		
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
