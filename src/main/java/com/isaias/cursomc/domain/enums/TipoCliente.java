ee431f9c993c4796f08ec05daafb203d5d8547ed
Isaias Neres Pinto <isaias.neres@yahoo.com>
2019-04-17 23:25:48 -0300

    Cliente, TipoCliente, telefones e endereços

---
+++ src/main/java/com/isaias/cursomc/domain/enums/TipoCliente.java
@@ -0,0 +1,41 @@
+package com.isaias.cursomc.domain.enums;
+
+public enum TipoCliente {
+	
+	PESSOAFISICA(1, "Pessoa Física"),
+	PESSOAJURIDICA(2, "Pessoa Jurídica");
+	
+	private int cod;
+	private String descricao;
+	
+	// o construtor do tipo enumerado é privado
+	private TipoCliente(int cod, String descricao) {
+		this.cod = cod;
+		this.descricao = descricao;
+	}
+
+	public int getCod() {
+		return cod;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	// static possibilita executar o metodo sem instanciar o objeto.
+	public static TipoCliente toEnum(Integer cod) {
+		
+		if(cod == null) {
+			return null;
+		}
+		
+		for(TipoCliente x : TipoCliente.values()) {
+			if(cod.equals(x.getCod())) {
+				return x;
+			}
+		}
+		
+		throw new IllegalArgumentException("Id inválido: " + cod);
+	}
+
+}
---
+++ src/main/java/com/isaias/cursomc/repositories/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.isaias.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.isaias.cursomc.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+}
