cf7d457bc27bc80fcb5691990033f90eddd2e041
Isaias Neres Pinto <isaias.neres@yahoo.com>
2019-04-28 20:35:03 -0300

    Validação sintática com bean validation

--- src/main/java/com/isaias/cursomc/services/CategoriaService.java
+++ src/main/java/com/isaias/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.isaias.cursomc.domain.Categoria;
+import com.isaias.cursomc.dto.CategoriaDTO;
 import com.isaias.cursomc.repositories.CategoriaRepository;
 import com.isaias.cursomc.services.exceptions.DataIntegrityException;
 import com.isaias.cursomc.services.exceptions.ObjectNotFoundException;
@@ -55,6 +56,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 		
 
