49e050249445b5f2fd95c5acf9deaf395e9c6b36
Isaias Neres Pinto <isaias.neres@yahoo.com>
2019-05-02 22:12:55 -0300

    Inserindo um novo Cliente com POST

--- src/main/java/com/isaias/cursomc/domain/Cliente.java
+++ src/main/java/com/isaias/cursomc/domain/Cliente.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
@@ -30,7 +31,8 @@ public class Cliente implements Serializable{
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
-	@OneToMany(mappedBy="cliente")
+	// toda e qualquer operação afeta os muitos endereços
+	@OneToMany(mappedBy="cliente", cascade = CascadeType.ALL)
 	private List<Endereco> enderecos = new ArrayList<>();
 	
 	// Set é um conjunto que não aceita repetição
--- src/main/java/com/isaias/cursomc/services/ClienteService.java
+++ src/main/java/com/isaias/cursomc/services/ClienteService.java
@@ -58,7 +58,7 @@ public class ClienteService {
 			repo.deleteById(id);
 		}
 		catch(DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possível excluir um cliente que possui endereços");
+			throw new DataIntegrityException("Não é possível excluir um cliente que possui pedidos");
 		}
 	}
 	
