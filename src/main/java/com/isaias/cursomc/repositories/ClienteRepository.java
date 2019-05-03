826b5a77bb6a6029ee7dcc753ba10a1ecb56153a
Isaias Neres Pinto <isaias.neres@yahoo.com>
2019-05-03 00:56:56 -0300

    Validação customizada: email não repetido na inserção de Cliente

--- src/main/java/com/isaias/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/isaias/cursomc/repositories/ClienteRepository.java
@@ -2,10 +2,16 @@ package com.isaias.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.isaias.cursomc.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+	
+	//Não precisa ser envolvida coma uma transação de banco de dados
+	// faz ela ficar mais rapida e evita o lock
+	@Transactional(readOnly = true)
+	Cliente findByEmail(String email);
 
 }
