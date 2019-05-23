5383cdc4e365adca054718f358e5189e1efcc7cd
Isaias Neres Pinto <isaias.neres@yahoo.com>
2019-05-23 01:17:18 -0300

    Esquecir a senha

--- src/main/java/com/isaias/cursomc/config/SecurityConfig.java
+++ src/main/java/com/isaias/cursomc/config/SecurityConfig.java
@@ -46,8 +46,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	};
 	
 	private static final String[] PUBLIC_MATCHERS_POST = {
-			"/clientes/**"
+			"/clientes/**",
+			"/auth/forgot/**"
 	};
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		
